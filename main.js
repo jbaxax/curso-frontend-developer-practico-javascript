02ee39b0ff1cf5fd3ccf9b776edc09c8ccc811a6
Walter Jave <walterjave92@gmail.com>
2023-08-08 22:14:11 -0500

    add my index.html, style,css and main,js

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 const email = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menu = document.querySelector('.menu');
+const carritoIcon = document.querySelector('.navbar-shopping-cart') 
 
-email.addEventListener(,toogleDesktopMenu)
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside= document.querySelector('.product-detail');
+
+
+
+email.addEventListener('click',toogleDesktopMenu);
+menu.addEventListener('click',toogleMobileMenu);
+carritoIcon.addEventListener('click',toogleDesktopAside)
 
 function toogleDesktopMenu(){
-    email.classList.toggle('inactive');
+    const asideClose = aside.classList.contains('inactive');
+    if(!asideClose){
+        aside.classList.toggle('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+}
+function toogleMobileMenu(){
+    const asideClose = aside.classList.contains('inactive');
+    if(!asideClose){
+        aside.classList.toggle('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+function toogleDesktopAside(){
+    const mobileClose = mobileMenu.classList.contains('inactive');
+    
+    if(!mobileClose){
+        mobileMenu.classList.toggle('inactive');       
+    }
+
+    aside.classList.toggle('inactive');
 }
