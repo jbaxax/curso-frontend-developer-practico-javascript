fe62923a0f09545d98ea8b94d6ca191525568d39
Walter Jave <walterjave92@gmail.com>
2023-08-09 16:37:03 -0500

    recodando productList

--- main.js
+++ main.js
@@ -1,37 +1,73 @@
 const email = document.querySelector('.navbar-email');
 const menu = document.querySelector('.menu');
-const carritoIcon = document.querySelector('.navbar-shopping-cart') 
+const carritoIcon = document.querySelector('.navbar-shopping-cart')
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside= document.querySelector('.product-detail');
+const aside = document.querySelector('.product-detail');
 
 
 
-email.addEventListener('click',toogleDesktopMenu);
-menu.addEventListener('click',toogleMobileMenu);
-carritoIcon.addEventListener('click',toogleDesktopAside)
+email.addEventListener('click', toogleDesktopMenu);
+menu.addEventListener('click', toogleMobileMenu);
+carritoIcon.addEventListener('click', toogleDesktopAside)
 
-function toogleDesktopMenu(){
+function toogleDesktopMenu() {
     const asideClose = aside.classList.contains('inactive');
-    if(!asideClose){
+    if (!asideClose) {
         aside.classList.toggle('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
-function toogleMobileMenu(){
+function toogleMobileMenu() {
     const asideClose = aside.classList.contains('inactive');
-    if(!asideClose){
+    if (!asideClose) {
         aside.classList.toggle('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
-function toogleDesktopAside(){
+function toogleDesktopAside() {
     const mobileClose = mobileMenu.classList.contains('inactive');
-    
-    if(!mobileClose){
-        mobileMenu.classList.toggle('inactive');       
+
+    if (!mobileClose) {
+        mobileMenu.classList.toggle('inactive');
     }
 
     aside.classList.toggle('inactive');
 }
+
+
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+            <div>
+                <p>$120,00</p>
+                <p>Bike</p>
+            </div>
+            <figure>
+                <img src="./icons/bt_add_to_cart.svg" alt="">
+            </figure>
+        </div>
+</div>  */
+
+productList = [];
+productList.push = ({
+    name: 'Bike',
+    price: 400,
+}),
+    productList.push = ({
+        name: 'Bike',
+        price: 400,
+    }),
+    productList.push = ({
+        name: 'Bike',
+        price: 400,
+    }),
+
+    function renderProducts(arr) {
+        for (product of arr) {
+
+        }
+    }
+
+renderProducts(productList);
