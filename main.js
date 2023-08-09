4d15ef47a03b721e4be7b2536721c62631b7433b
Walter Jave <walterjave92@gmail.com>
2023-08-08 20:24:42 -0500

    add progressing

---
+++ main.js
@@ -0,0 +1,8 @@
+const email = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+email.addEventListener(,toogleDesktopMenu)
+
+function toogleDesktopMenu(){
+    email.classList.toggle('inactive');
+}
